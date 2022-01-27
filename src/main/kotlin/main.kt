a9beb5b4f0032ae440885450a8298891bae60a16
Magdeev_Damir <magdeevdr@msk.bcs.ru>
2022-01-27 08:25:53 +0300

    afds

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,55 +4,48 @@ fun howMuch(paymentType: String = "vk", previousPayments: Int = 0, currentPaymen
     var previousPaymentsCents = 0
 
     if (previousPayments != 0) {
-
         previousPaymentsCents = previousPayments * 100
     }
 
     val currentPaymentCents = currentPayment * 100
 
 
-    var commision = 0
+    var commission = 0
 
     when (paymentType) {
 
         in "vk", "master", "maestro" -> {
 
-            commision = 0
+            commission = 0
 
             if (paymentType == "master" && paymentType == "maestro") {
 
                 if (currentPaymentCents >= 300__00) {
-
-                    commision = 0
+                    commission = 0
                 }
 
                 if ((currentPaymentCents + previousPaymentsCents) >= 75_000_00) {
-
-                    commision = 0
+                    commission = 0
 
                 }
+            }
+            else if (paymentType == "vk" || currentPaymentCents >= 15_000_00) {
+                commission = -1
             }
         }
 
         in "visa", "mir" -> {
 
-            commision = (currentPaymentCents / 100 * 0.75).toInt()
+            commission = (currentPaymentCents / 100 * 0.75).toInt()
 
-            if (commision <= 3500) {
-
-                commision = 3500
+            if (commission <= 3500) {
+                commission = 3500
 
             }
         }
     }
 
-    if (paymentType == "vk" || currentPaymentCents >= 15_000_00){
-
-        commision = -1
-
-    }
-
-    return commision
+    return commission
 
 }
 
@@ -63,9 +56,9 @@ fun main(){
     val paymentType = "vk"
     val previousPayments = 1500
 
-    val commision = howMuch(paymentType, previousPayments, payment)
+    val commission = howMuch(paymentType, previousPayments, payment)
 
-    if (commision < 0){
+    if (commission < 0){
 
         println("Operation denied, limit is reached")
 
